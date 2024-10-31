6d347aaf1461e98a943f8693b3ed5c77198008ae
Reinar Kajava <reinar.kajava@voco.ee>
2024-10-31 12:23:27 +0200

    read data from a file

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
-app.get('/',(req, res) => {
-    res.send('test')
-}) 
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
+
+app.get('/', (req, res) => {
+    fs.readFile('./tasks', 'utf-8', (err, data) => {
+        if(err) {
+            console.log(err)
+            return
+        } 
+        const tasks = data.split("\n") 
+        res.render('index', {tasks: tasks})
+    })
+})
 
 app.listen(3001, () => {
-    console.log('example app is started at http://localhost:3001')
+    console.log('Server started at localhost:3001')
 })
