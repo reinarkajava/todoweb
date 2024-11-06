43191b0edc3a388747a3674d08999b671ef3989b
Reinar Kajava <reinar.kajava@voco.ee>
2024-11-06 12:49:14 +0200

    Writing and viewing data on website

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 
+const readFile = (filename) =>{ 
+    return new Promise((resolve, reject) =>{
+        fs.readFile('./tasks', 'utf-8', (err, data) => {
+            if(err) {
+                console.log(err)
+                return
+            } 
+            const tasks = data.split("\n") 
+            resolve(tasks)
+        })
+    })
+}
 
 app.get('/', (req, res) => {
     fs.readFile('./tasks', 'utf-8', (err, data) => {
@@ -18,24 +30,30 @@ app.get('/', (req, res) => {
         } 
         const tasks = data.split("\n") 
         res.render('index', {tasks: tasks})
+        console.log(tasks)
 
     })
 })
 
 
 app.post('/', (req, res) => {
-    console.log('form sent data')
-    console.log('req body ', req.body.tasks)
-    fs.readFile('./tasks','utf8',(err, data) => {
-        if (err) {
-            console.log(err)
-            return;
-        }
-        const tasks = data.split("\n")
+    readFile('./tasks')
+        .then(tasks => {
+            tasks.push(req.body.task)
+            const data = tasks.join("\n")
+            console.log(data)
+            fs.writeFile('./tasks', data, err => { 
+                if(err) {
+                    console.error(err)
+                    return
+                } 
+                res.redirect('/')
+            })
+        })
+
 
     }) 
 
-}) 
 app.listen(3001, () => {
     console.log('Server started at localhost:3001')
 })
