78515fad33480fb6eeaae9f41fe594f24e5604fe
Reinar Kajava <reinar.kajava@voco.ee>
2024-11-22 13:00:50 +0200

    <pre>Veateadete näitamine, Lehe tõlkimine eesti keelde

--- index.js
+++ index.js
@@ -36,37 +36,45 @@ const writeFile = (filename, data) => {
 }
 
 app.get('/', (req, res) => {
-    let error = null
-    if(req.body.task.trim().length == 0){
-        error= 'Please insert correct task data'
-    }  
     readFile('./tasks.json')
       .then((tasks) => {
-        res.render('index', {tasks: tasks, error:error})
-
+        res.render('index', {
+            tasks: tasks,
+            error: null
+        })
     })
 })
 
-
-
 app.post('/', (req, res) => {
-    readFile('./tasks.json')
-      .then((tasks) => {
-        let index
-        if(tasks.length === 0){
-            index = 0
-        } else {
-            index = tasks[tasks.length - 1].id + 1
-        }
-        const newTask = {
-            id: index,
-            task: req.body.task
-        }
-        tasks.push(newTask)
-        const data = JSON.stringify(tasks, null, 2)
-        writeFile('./tasks.json', data)
-        res.redirect('/')
-    });
+    let error = null
+    if(req.body.task.trim().length == 0){
+        error = 'Please insert correct task data'
+        readFile('./tasks.json')
+        .then((tasks) => {
+            res.render('index', {
+                tasks: tasks,
+                error: error
+            })
+        })
+    } else { 
+        readFile('./tasks.json')
+        .then((tasks) => {
+            let index
+            if(tasks.length === 0){
+                index = 0
+            } else {
+                index = tasks[tasks.length - 1].id + 1
+            }
+            const newTask = {
+                id: index,
+                task: req.body.task
+            }
+            tasks.push(newTask)
+            const data = JSON.stringify(tasks, null, 2)
+            writeFile('./tasks.json', data)
+            res.redirect('/')
+        });
+    }
 })
 
 app.get('/delete-task/:taskId', (req, res) => {
@@ -82,6 +90,12 @@ app.get('/delete-task/:taskId', (req, res) => {
             writeFile('./tasks.json', data)
             res.redirect('/')
         })
+})
+
+app.get('/delete-tasks', (req, res) => {
+    const data = JSON.stringify([], null, 2)
+    writeFile('./tasks.json', data)
+    res.redirect('/')
 })
 
 
